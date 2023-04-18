772890687fc059e53e31669eb54137e93c8da5f7
Sasha <sashla1713@gmail.com>
2023-04-18 14:01:35 +0300

    Added limit on list items and save list on reload

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const addBtn = document.querySelector('.button__add'),
     removeBtn = document.querySelector('.button__remove'),
     input = document.querySelector('.queue__input'),
     queue = document.querySelector('.queue__list'),
-    queueMsg = document.querySelector('.queue__message')
+    queueMsg = document.querySelector('.queue__message'),
+    myAge = 23;
+    listItems = [];
 
+let count = listItems.length;
+    
 function isEmptyQueue(){
-    if (queue.childElementCount < 1){
+    if (listItems.length < 1){
         queueMsg.classList.remove('hidden')
         queue.classList.add('hidden')
         return true
@@ -16,16 +20,28 @@ function isEmptyQueue(){
     }
 }
 
+function addItem(item){
+    listItems.push(item);
+    const queueItem = document.createElement('li');
+    queueItem.classList.add('queue__list--item');
+    queueItem.textContent = item;
+    queue.appendChild(queueItem);
+}
+
 function handleAdding(){
+    
     addBtn.addEventListener('click', () => {
-        if (input.value){
-            const queueItem = document.createElement('li');
-            queueItem.classList.add('queue__list--item');
-            queueItem.textContent = input.value;
-            queue.appendChild(queueItem);
+        inputValue = input.value.trim();
+        if (inputValue && count < myAge){
+            addItem(inputValue)
             input.value = '';
+            count++;
             isEmptyQueue();
+            localStorage.setItem('listItems', JSON.stringify(listItems));
         }
+        else if (count >= myAge) {
+            alert(`You can't add more than ${myAge} items to the queue :(`);
+          }
         else{
             alert('You can`t add to the queue an empty item')
         }
@@ -39,9 +55,21 @@ function handleRemoving(){
         }
         else{
             queue.removeChild(queue.firstChild);
-            isEmptyQueue()
+            listItems.shift();
+            count--;
+            isEmptyQueue();
+            localStorage.setItem('listItems', JSON.stringify(listItems));
         }
     })
+}
+
+if (localStorage.getItem('listItems')) {
+    const storedItems = JSON.parse(localStorage.getItem('listItems'));
+    for (let item of storedItems) {
+        addItem(item)
+    }
+    count = listItems.length;
+    isEmptyQueue();
 }
 
 isEmptyQueue();
