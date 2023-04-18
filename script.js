5aa355a793ac4219e06840862edb6d6805dce37c
Oleksandra <sashla1713@gmail.com>
2023-04-18 22:50:47 +0300

    Code refactoring

--- script.js
+++ script.js
@@ -1,78 +1,68 @@
-const addBtn = document.querySelector('.button__add'),
-    removeBtn = document.querySelector('.button__remove'),
-    input = document.querySelector('.queue__input'),
-    queue = document.querySelector('.queue__list'),
-    queueMsg = document.querySelector('.queue__message'),
-    myAge = 23;
-    listItems = [];
+const addBtn = document.querySelector('.button__add'), 
+    removeBtn = document.querySelector('.button__remove'), 
+    input = document.querySelector('.queue__input'), 
+    queue = document.querySelector('.queue__list'), 
+    queueMsg = document.querySelector('.queue__message'), 
+    listItems = JSON.parse(localStorage.getItem("listItems")) || []; 
+    myAge = 23; 
 
-let count = listItems.length;
-    
-function isEmptyQueue(){
-    if (listItems.length < 1){
-        queueMsg.classList.remove('hidden')
-        queue.classList.add('hidden')
-        return true
-    } else{
-        queueMsg.classList.add('hidden')
+function hideQueue(){
+    if(!listItems.length){
+        queueMsg.classList.remove('hidden') 
+        queue.classList.add('hidden') 
+    }
+} 
+
+function showQueue(){
+    if(listItems.length){
+        queueMsg.classList.add('hidden') 
         queue.classList.remove('hidden')
-        return false
     }
+} 
+
+function renderQueue(){ 
+    hideQueue()
+    listItems.map(item => addItem(item)) 
+    showQueue() 
+} 
+
+function addItem(item){ 
+    const queueItem = document.createElement('li'); 
+    queueItem.classList.add('queue__list--item'); 
+    queueItem.textContent = item; 
+    queue.appendChild(queueItem); 
+} 
+
+function handleAdding(){ 
+    inputValue = input.value.trim();
+
+    if (inputValue && listItems.length < myAge){ 
+        listItems.push(inputValue); 
+        addItem(inputValue)
+        showQueue()
+        input.value = ''; 
+        localStorage.setItem('listItems', JSON.stringify(listItems)); 
+    } 
+    else if (listItems.length >= myAge) { 
+        alert(`You can't add more than ${myAge} items to the queue :(`); 
+    } 
+    else{ 
+        alert('You can`t add to the queue an empty item') 
+    } 
+} 
+
+function handleRemoving(){ 
+    if(listItems.length < 1){ 
+        alert('You can`t remove anything from the empty queue') 
+    } 
+    else{ 
+        queue.removeChild(queue.firstChild); 
+        listItems.shift(); 
+        localStorage.setItem('listItems', JSON.stringify(listItems)); 
+        hideQueue() 
+    } 
 }
 
-function addItem(item){
-    listItems.push(item);
-    const queueItem = document.createElement('li');
-    queueItem.classList.add('queue__list--item');
-    queueItem.textContent = item;
-    queue.appendChild(queueItem);
-}
-
-function handleAdding(){
-    
-    addBtn.addEventListener('click', () => {
-        inputValue = input.value.trim();
-        if (inputValue && count < myAge){
-            addItem(inputValue)
-            input.value = '';
-            count++;
-            isEmptyQueue();
-            localStorage.setItem('listItems', JSON.stringify(listItems));
-        }
-        else if (count >= myAge) {
-            alert(`You can't add more than ${myAge} items to the queue :(`);
-          }
-        else{
-            alert('You can`t add to the queue an empty item')
-        }
-    } )
-}
-
-function handleRemoving(){
-    removeBtn.addEventListener('click', () => {
-        if(isEmptyQueue()){
-            alert('You can`t remove anything from the empty queue')
-        }
-        else{
-            queue.removeChild(queue.firstChild);
-            listItems.shift();
-            count--;
-            isEmptyQueue();
-            localStorage.setItem('listItems', JSON.stringify(listItems));
-        }
-    })
-}
-
-if (localStorage.getItem('listItems')) {
-    const storedItems = JSON.parse(localStorage.getItem('listItems'));
-    for (let item of storedItems) {
-        addItem(item)
-    }
-    count = listItems.length;
-    isEmptyQueue();
-}
-
-isEmptyQueue();
-handleAdding();
-handleRemoving();
-
+addBtn.addEventListener('click', handleAdding);
+removeBtn.addEventListener('click', handleRemoving)
+renderQueue();
